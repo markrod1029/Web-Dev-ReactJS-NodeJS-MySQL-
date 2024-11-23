9e3a49eac6d308bbc8994c258ebd7678d48c10b7
markrod1029 <markrodcadayong17@gmail.com>
2024-11-23 22:05:14 +0800

    GET and POST Requests

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
 const db = require('./models');
+
+// Routers
+const postRouter = require('./routes/Posts');
+app.use("/posts", postRouter);
+
 
 db.sequelize.sync()
 .then(() => {
@@ -9,6 +15,6 @@ db.sequelize.sync()
         console.log('Server running on port 3001')
     });
 })
-.catch((err) => {
-    console.error('Failed to sync database:', err);
-});
+// .catch((err) => {
+//     console.error('Failed to sync database:', err);
+// });
